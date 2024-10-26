d3b3753fac83fae3ac295efd21d3ab16fdd59aef
Flora Nwakpa <floranwakpa@gmail.com>
2024-10-26 18:52:24 +0100

    added functionalities

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const darkThemeBtn = document.getElementById('dark-button')
-const lightThemeBtn = document.getElementById('light-button')
-const calculatorWrapper = document.getElementById('calculator-wrapper')
+const darkThemeBtn = document.getElementById('dark-button');
+const lightThemeBtn = document.getElementById('light-button');
+const calculatorWrapper = document.getElementById('calculator-wrapper');
+
+const display = document.getElementById('display')
 
 // Functions to toggle theme
 
@@ -18,3 +20,21 @@ function enableLightTheme () {
 
 darkThemeBtn.addEventListener('click', enableDarkTheme)
 lightThemeBtn.addEventListener('click', enableLightTheme)
+
+// Set display
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = ''
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
